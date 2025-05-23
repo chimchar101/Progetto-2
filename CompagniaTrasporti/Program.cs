de8c23a3d6bf03be55159827d153fce2f6db6d40
chimchar101 <simoneaddesso25@gmail.com>
2025-05-23 17:49:15 +0200

    Update Program.cs

--- CompagniaTrasporti/Program.cs
+++ CompagniaTrasporti/Program.cs
@@ -42,4 +42,57 @@ public class Program
             }
         } while (!esci);
     }
+
+    private static void AggiungiAutista(List<Dipendente> dipendenti)
+    {
+        Console.WriteLine("Inserisci il nome dell'autista:");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Quanti anni ha?");
+        int eta = int.Parse(Console.ReadLine());
+        Console.WriteLine("Inserisci la patente dell'autista:");
+        string patente = Console.ReadLine();
+
+        Autista autista = new Autista(nome, eta, patente);
+        dipendenti.Add(autista);
+
+        Console.WriteLine("Autista inserito!");
+    }
+
+    private static void AggiungiMeccanico(List<Dipendente> dipendenti)
+    {
+        Console.WriteLine("Inserisci il nome del meccanico:");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Quanti anni ha?");
+        int eta = int.Parse(Console.ReadLine());
+        Console.WriteLine("Inserisci la specializzazione dell'autista:");
+        string specializzazione = Console.ReadLine();
+
+        Meccanico meccanico = new Meccanico(nome, eta, specializzazione);
+        dipendenti.Add(meccanico);
+
+        Console.WriteLine("Meccanico inserito!");
+    }
+
+    private static void AggiungiOperatore(List<Dipendente> dipendenti)
+    {
+        Console.WriteLine("Inserisci il nome del meccanico:");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Quanti anni ha?");
+        int eta = int.Parse(Console.ReadLine());
+        Console.WriteLine("Che turno fa? (Giorno/Notte)");
+        string turno = Console.ReadLine();
+
+        OperatoreCentrale operatore = new OperatoreCentrale(nome, eta, turno);
+        dipendenti.Add(operatore);
+
+        Console.WriteLine("Operatore centrale inserito!");
+    }
+
+    private static void StampaDipendenti(List<Dipendente> dipendenti)
+    {
+        foreach (Dipendente dip in dipendenti)
+        {
+            Console.WriteLine($"Nome: {dip.Nome}, Turno: {dip.Turno}, Tipo: {dip.GetType().Name}");
+        }
+    }
 }
